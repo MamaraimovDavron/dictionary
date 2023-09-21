66de917fe9e6712b3a1c1f30c3b1f3a269594a86
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-09-21 14:48:58 +0500

    audio also done

--- script.js
+++ script.js
@@ -16,15 +16,26 @@ const getWords = async () => {
       `https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`
     );
 
-    const link = document.createElement("a");
+    // const link = document.createElement('')
+    const audioLink = document.createElement("audio");
+
+    // audioLink.setAttribute(controls);
     const Url = document.createElement("a");
 
     result = res.data[0].meanings[0].definitions[0].definition;
     meanings.innerHTML = result;
     phonetic.innerHTML = res.data[0].phonetic;
 
-    link.innerHTML = `<a href="${res.data[0].phonetics[0].audio}">${res.data[0].phonetics[0].audio}</a>`;
-    phonetics.appendChild(link);
+    phonetics.innerHTML = `
+
+        <audio controls>
+            <source src="${res.data[0].phonetics[0].audio}" type="audio/ogg" />
+            <source src="${res.data[0].phonetics[0].audio}" type="audio/mpeg" />
+        </audio>
+    `;
+    // phonetics.appendChild(audioLink);
+    // link.innerHTML = `<a href="${res.data[0].phonetics[0].audio}">${res.data[0].phonetics[0].audio}</a>`;
+    // phonetics.appendChild(link);
 
     sourceUrl.innerHTML = `<a href="${res.data[0].sourceUrls[0]}">${res.data[0].sourceUrls[0]}</a>`;
     sourceUrl.appendChild(Url);
